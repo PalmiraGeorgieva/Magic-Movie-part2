e0fcd2c47967dc4443ac4462694d4dbb87180848
Palmira Georgieva <palmira.1993@abv.bg>
2026-07-20 13:54:25 +0300

    add authController

--- src/routs.js
+++ src/routs.js
@@ -2,13 +2,16 @@ import { Router } from 'express';
 import homeController from './controllers/homeControllers.js';
 import movieController from './controllers/movieController.js';
 import artistController from './controllers/artistsController.js';
+import authController from './controllers/authController.js';
 
 const routes = Router();
 
 routes.use('/', homeController);
 routes.use('/movies', movieController);
 routes.use('/artists', artistController);
+routes.use('/auth', authController)
 routes.get('*url', (req, res) => {
     res.render('404', { title: 'Page Not Found' });
 });
+
 export default routes;
